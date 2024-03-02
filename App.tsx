bbbe30b4a5fb3ca7988578ac887ccfa6ade871f3
monank sojitra <monaksojitra4444@gmail.com>
2024-03-02 20:20:23 +0530

    [add] tab

--- App.tsx
+++ App.tsx
@@ -1,24 +1,19 @@
-import {
-  Image,
-  SafeAreaView,
-  SafeAreaViewBase,
-  SafeAreaViewComponent,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
 import React from "react";
-import Splash from "./screens/Splash";
-import Login from "./screens/Login";
 import Home from "./screens/Home";
+import Login from "./screens/Login";
+import { SafeAreaView } from "react-native";
 
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createNativeStackNavigator();
 const App = () => {
   return (
-    <SafeAreaView>
-      {/* <Splash /> */}
-      {/* <Login /> */}
-      <Home />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,11 +1,65 @@
-import { TouchableOpacity, View } from "react-native";
-import React from "react";
-
+import {
+  TouchableOpacity,
+  View,
+  Text,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
 const Home = () => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const foodItems = [
+    "Foods",
+    "Drinks",
+    "Snacks",
+    "Sauce",
+    "Foods",
+    "Drinks",
+    "Snacks",
+    "Sauce",
+  ];
   return (
-    <View className="h-full">
+    <View className="h-full p-10">
+      <View className="flex flex-row justify-between items-center">
+        <TouchableOpacity>
+          <Ionicons name="menu" size={32} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Ionicons name="cart-outline" size={32} />
+        </TouchableOpacity>
+      </View>
+      <View className="w-[70%] py-10">
+        <Text className="text-4xl font-bold">Delicious food for you</Text>
+      </View>
+      <View className="flex flex-row gap-x-3 items-center h-12 bg-white/70 rounded-full">
+        <Ionicons name="search" size={24} />
+        <TextInput placeholder="Search" className="text-base w-full" />
+      </View>
+
       <View>
-        <TouchableOpacity></TouchableOpacity>
+        <ScrollView horizontal className="flex gap-x-10">
+          {foodItems.map((item, i) => (
+            <TouchableOpacity
+              onPress={() => setSelectedIndex(i)}
+              className="py-3"
+              key={i}
+            >
+              <Text
+                className={`${
+                  selectedIndex === i ? "text-originPrimary" : "text-black/50"
+                } px-5 py-3`}
+              >
+                {item}
+              </Text>
+              <View
+                className={
+                  selectedIndex === i ? "h-1 w-full bg-originPrimary" : " "
+                }
+              />
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
       </View>
     </View>
   );
